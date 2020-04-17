764deefc6eb44e04f81bd2e8b28505d99de731fd
Yogesh Jagdale <yog99mail@gmail.com>
2020-04-17 15:08:39 +0530

    created test server

--- node-server/server.js
+++ node-server/server.js
@@ -1,1 +1,9 @@
 // node server
+var http = require('http');
+var server = http.createServer(function(req, res){
+    res.setHeader('content-type', 'text/html');
+    res.end(`[${req.method}] ${req.url}`)
+});
+
+server.listen(1000)
+console.log('server created successfuly!')
